03ff42017055f60f3c2d604a14dc6f07fefc09c3
Jason Monsef <jasonmonsef@gmail.com>
2015-02-02 12:10:47 -0800

    map link

--- wp-content/themes/botosushi/inc/modules/aboutus/aboutus.php
+++ wp-content/themes/botosushi/inc/modules/aboutus/aboutus.php
@@ -4,7 +4,7 @@
 		<h3>ADDRESS</h3>
 		<p>
 			11835 Carmel Mountain Rd<br> 
-			San Diego, CA 92128 - <a href="#">Map it!</a><br>
+			San Diego, CA 92128 - <a href="https://www.google.com/maps/place/11835+Carmel+Mountain+Rd,+Carmel+Mountain+Ranch+Town+Center,+San+Diego,+CA+92128/@32.9798454,-117.0771351,17z/data=!3m1!4b1!4m2!3m1!1s0x80dbfa018664db7d:0xcc402e2514645ee1" target="_blank">Map it!</a><br>
 			Call Us - 858.451.7800
 		</p>
 	</div>
